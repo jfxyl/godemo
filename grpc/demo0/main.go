59e1a0240354c817a2973b5e517be27b89cdc329
jfxy <yjj19941226@163.com>
2023-04-18 21:43:50 +0800

    grpc demo

--- grpc/demo0/main.go
+++ grpc/demo0/main.go
@@ -7,6 +7,7 @@ import (
 	pb "godemo/grpc/proto/person"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/timestamppb"
 	"log"
 	"net"
@@ -17,6 +18,31 @@ var (
 	server *grpc.Server
 	client pb.PersonServiceClient
 )
+
+type AuthInterceptor struct{}
+
+func (a *AuthInterceptor) UnaryInterceptorfunc(c context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+	fmt.Println("info.FullMethod", info.FullMethod)
+	check := func() (err error) {
+		md, ok := metadata.FromIncomingContext(c)
+		if !ok {
+			return fmt.Errorf("no auth info")
+		}
+		if len(md["authorization"]) == 0 {
+			return fmt.Errorf("no auth info")
+		}
+		if md["authorization"][0] != "token123" {
+			return fmt.Errorf("invalid token")
+		}
+		return nil
+	}
+	if info.FullMethod == "/person.PersonService/GetPerson" {
+		if err := check(); err != nil {
+			return nil, err
+		}
+	}
+	return handler(c, req)
+}
 
 type PersonService struct {
 	pb.UnimplementedPersonServiceServer
@@ -54,13 +80,51 @@ func (p *PersonService) GetPerson(ctx context.Context, req *pb.GetPersonRequest)
 	return person, nil
 }
 
+func (p *PersonService) ListPerson(ctx context.Context, req *pb.ListPersonRequest) (*pb.ListPersonResponse, error) {
+	var (
+		now    time.Time = time.Now()
+		person *pb.Person
+		resp   *pb.ListPersonResponse
+	)
+	person = &pb.Person{
+		Id:       1,
+		Name:     "jfxy",
+		Gender:   1,
+		Birthday: "1994-12-26",
+		Avatar:   "",
+		Email:    "",
+		Phone:    "135********",
+		Address: map[string]string{
+			"province": "江苏省",
+			"city":     "南京市",
+			"district": "玄武区",
+		},
+		Tags: []string{},
+		//Tags: []string{"golang", "php"},
+		CreatedAt: &timestamppb.Timestamp{
+			Seconds: now.Unix(),
+			Nanos:   int32(now.Nanosecond()),
+		},
+		//CreatedAt:timestamppb.Now(),
+	}
+
+	resp = &pb.ListPersonResponse{
+		Persons: []*pb.Person{
+			person,
+			person,
+		},
+	}
+	return resp, nil
+}
+
 func main() {
 	var (
-		err       error
-		port      = ":8080"
-		conn      *grpc.ClientConn
-		resp      *pb.Person
-		jsonBytes []byte
+		err        error
+		port       = ":8080"
+		conn       *grpc.ClientConn
+		person     *pb.Person
+		listPerson *pb.ListPersonResponse
+		jsonBytes  []byte
 	)
 	//启动服务端
 	go runServer(port)
@@ -75,15 +139,25 @@ func main() {
 	client = pb.NewPersonServiceClient(conn)
 
 	//调用GetPerson
-	resp, err = client.GetPerson(context.Background(), &pb.GetPersonRequest{
+	person, err = client.GetPerson(context.Background(), &pb.GetPersonRequest{
 		Id: 1,
 	})
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+	} else {
+		//proto.Marshal和proto.Unmarshal会导致空切片变成nil
+		jsonBytes, err = json.Marshal(person)
+		fmt.Println(string(jsonBytes))
 	}
-	//proto.Marshal和proto.Unmarshal会导致空切片变成nil
-	jsonBytes, err = json.Marshal(resp)
-	fmt.Println(string(jsonBytes))
+	//调用ListPerson
+	listPerson, err = client.ListPerson(context.Background(), &pb.ListPersonRequest{})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		//proto.Marshal和proto.Unmarshal会导致空切片变成nil
+		jsonBytes, err = json.Marshal(listPerson)
+		fmt.Println(string(jsonBytes))
+	}
 }
 
 func runServer(port string) {
@@ -94,7 +168,9 @@ func runServer(port string) {
 	if lis, err = net.Listen("tcp", port); err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	server = grpc.NewServer()
+	server = grpc.NewServer(
+		grpc.UnaryInterceptor((&AuthInterceptor{}).UnaryInterceptorfunc),
+	)
 	pb.RegisterPersonServiceServer(server, &PersonService{})
 	if err := server.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
